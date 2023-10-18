f472031fb8f696b0fd4d9e3c98553023b49b0a65
syedAfnan-24 <syedliyaquathafnan@gmail.com>
2023-10-18 13:26:31 +0530

    learning TypeScript, added adminSetting.ts...by converting previously existing adminLogin.js...which complied and modified adminLogin.js

--- scripts/adminLogin.ts
+++ scripts/adminLogin.ts
@@ -1,30 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var loginForm = document.getElementById("adminLogin");
+document.addEventListener("DOMContentLoaded", function() {
+    const loginForm = document.getElementById("adminLogin") as HTMLFormElement;
+
     if (loginForm) {
-        loginForm.addEventListener("submit", function (event) {
+        loginForm.addEventListener("submit", function(event: Event) {
             event.preventDefault();
-            var storedUserString = localStorage.getItem("admin");
+
+            const storedUserString = localStorage.getItem("admin");
             if (storedUserString) {
-                var storedUser = JSON.parse(storedUserString);
-                var enteredUsername = document.getElementById("aname").value;
-                var enteredPassword = document.getElementById("apass").value;
+                const storedUser = JSON.parse(storedUserString);
+                const enteredUsername = (document.getElementById("aname") as HTMLInputElement).value;
+                const enteredPassword = (document.getElementById("apass") as HTMLInputElement).value;
+
                 if (storedUser && storedUser.adminName === enteredUsername && storedUser.password === enteredPassword) {
                     // Redirect to user details page after successful login
                     sessionStorage.setItem("admin", storedUser.adminName);
                     window.location.href = "adminHome.html";
-                }
-                else {
+                } else {
                     alert("Invalid username or password. Please try again.");
                     // Optionally, you can clear the input fields after an unsuccessful login attempt
                     // loginForm.reset();
                 }
-            }
-            else {
+            } else {
                 alert("Admin credentials not found. Please sign up.");
             }
         });
-    }
-    else {
+    } else {
         console.error("Element with ID 'adminLogin' not found.");
     }
 });
