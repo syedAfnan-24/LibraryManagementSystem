b9b160ccbf6a0e4202f11569ff47164e58b2a34d
syedAfnan-24 <syedliyaquathafnan@gmail.com>
2023-10-18 17:48:55 +0530

    reverted changes, because I should be creating a new repo for typescript and stencil project

--- scripts/adminLogin.js
+++ scripts/adminLogin.js
@@ -1,30 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var loginForm = document.getElementById("adminLogin");
-    if (loginForm) {
-        loginForm.addEventListener("submit", function (event) {
-            event.preventDefault();
-            var storedUserString = localStorage.getItem("admin");
-            if (storedUserString) {
-                var storedUser = JSON.parse(storedUserString);
-                var enteredUsername = document.getElementById("aname").value;
-                var enteredPassword = document.getElementById("apass").value;
-                if (storedUser && storedUser.adminName === enteredUsername && storedUser.password === enteredPassword) {
-                    // Redirect to user details page after successful login
-                    sessionStorage.setItem("admin", storedUser.adminName);
-                    window.location.href = "adminHome.html";
-                }
-                else {
-                    alert("Invalid username or password. Please try again.");
-                    // Optionally, you can clear the input fields after an unsuccessful login attempt
-                    // loginForm.reset();
-                }
-            }
-            else {
-                alert("Admin credentials not found. Please sign up.");
-            }
-        });
-    }
-    else {
-        console.error("Element with ID 'adminLogin' not found.");
+const loginForm = document.getElementById("adminLogin");
+
+loginForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+   
+    const storedUser = JSON.parse(localStorage.getItem("admin"));
+    const enteredUsername = document.getElementById("aname").value; 
+    const enteredPassword = document.getElementById("apass").value;
+
+    if (storedUser && storedUser.adminName === enteredUsername && storedUser.password === enteredPassword) {
+        // Redirect to user details page after successful login
+        sessionStorage.setItem("admin",storedUser.adminName)
+        window.location.href = "adminHome.html";
+    } else {
+        alert("Invalid username or password. Please try again.");
+        // Optionally, you can clear the input fields after unsuccessful login attempt
+        // loginForm.reset();
     }
 });
